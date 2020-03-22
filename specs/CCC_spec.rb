c5d656f78d4773239b84256503ea9d55519f9448
Jack Quigley <jackaquigley@gmail.com>
2020-03-22 15:15:10 +0000

    song has artist and title

--- specs/CCC_spec.rb
+++ specs/CCC_spec.rb
@@ -3,14 +3,22 @@ require('minitest/reporters')
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
-require ('../CCC.rb')
+require_relative ('../CCC.rb')
 
 #create a song
 
-Class TestSong < Minitest::Test
+class TestSong < Minitest::Test
 
   def setup
-    @song = Song.new("New York, New York", "Frank Sinatra")
+    @song = Song.new("Frank Sinatra", "New York, New York")
   end
 
+  def test_song_has_title
+    assert_equal("New York, New York", @song.title)
+  end
+
+  def test_song_has_artist
+    assert_equal("Frank Sinatra", @song.artist)
+  end
+  
 end
